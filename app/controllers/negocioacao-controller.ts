ceedfb333515dd1a1e8037d24e547e21d406c10c
brenasarzi <sarzibrena@gmail.com>
2022-09-01 17:07:10 -0300

    tipagem de array

--- app/controllers/negocioacao-controller.ts
+++ app/controllers/negocioacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js"
+import { Negociacoes } from "../models/negociacoes.js"
 
 export class NegociacaoController {
     private inputData: HTMLInputElement
     private inputQuantidade: HTMLInputElement 
     private inputValor: HTMLInputElement
+    private negociacoes: Negociacoes = new Negociacoes()
 
     constructor(){
         this.inputData = document.querySelector('#data')
@@ -12,7 +14,9 @@ export class NegociacaoController {
     }
     adiciona(): void /* não retorna nada */{
         const negociacao = this.criaNegociacao()
-        console.log(negociacao)
+        this.negociacoes.adiciona(negociacao)
+        this.negociacoes.lista().pop()
+        console.log(this.negociacoes.lista())
         this.limparFormulario()
     }
     criaNegociacao(): Negociacao {
